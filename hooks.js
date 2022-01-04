ebcd075699cd41a943aa03f677425004b6268bdb
Richard Hansen <rhansen@rhansen.org>
2022-01-03 20:10:39 -0500

    Fix `eejs.require()` paths

--- hooks.js
+++ hooks.js
@@ -3,21 +3,21 @@
 const eejs = require('ep_etherpad-lite/node/eejs');
 
 exports.eejsBlock_editbarMenuLeft = (hookName, args, cb) => {
-  args.content += eejs.require('ep_mathjax/templates/editbarButtons.ejs', {}, module);
+  args.content += eejs.require('./templates/editbarButtons.ejs', {}, module);
   return cb();
 };
 
 exports.eejsBlock_editorContainerBox = (hookName, args, cb) => {
-  args.content += eejs.require('ep_mathjax/templates/modals.ejs', {}, module);
+  args.content += eejs.require('./templates/modals.ejs', {}, module);
   return cb();
 };
 
 exports.eejsBlock_scripts = (hookName, args, cb) => {
-  args.content += eejs.require('ep_mathjax/templates/scripts.ejs', {}, module);
+  args.content += eejs.require('./templates/scripts.ejs', {}, module);
   return cb();
 };
 
 exports.eejsBlock_styles = (hookName, args, cb) => {
-  args.content += eejs.require('ep_mathjax/templates/styles.ejs', {}, module);
+  args.content += eejs.require('./templates/styles.ejs', {}, module);
   return cb();
 };
